0a737b4ab74d9f48b6607e92ab03d9e1f839d594
Nan <nanwang2016@gmail.com>
2017-03-27 16:58:40 +0800

    Initial Commit

---
+++ EnigmaPod/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EnigmaPod
+//
+//  Created by Nan Wang on 27/3/17.
+//  Copyright © 2017 Nan Wang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EnigmaPod/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EnigmaPod
+//
+//  Created by Nan Wang on 27/3/17.
+//  Copyright © 2017 Nan Wang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
